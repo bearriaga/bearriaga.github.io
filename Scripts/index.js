f771f2726bf107cf8db6c4f2b3b7997ddffbf794
Bryan Arriaga <turro92@hotmail.com>
2022-06-11 22:03:53 -0400

    Restarted project as vue project

--- Scripts/index.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('.collapse').on('hide.bs.collapse', function () {
-        $(this).prev().children('.collapseIcon').html('+');
-    })
-    $('.collapse').on('show.bs.collapse', function () {
-        $(this).prev().children('.collapseIcon').html('-');
-    })
-});
