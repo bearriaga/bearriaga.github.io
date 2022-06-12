8690ff03dcd1ac0465530d981414e9e57e817bc8
Bryan Arriaga <turro92@hotmail.com>
2022-06-12 16:25:46 -0400

    Added validation to add xp entry dialog Moved delete logic to XPSection

--- src/components/XPEntryListItem.vue
+++ src/components/XPEntryListItem.vue
@@ -3,7 +3,7 @@
         <v-row>
             <v-col cols="6">
                 <v-text-field label="Amount" v-model="amount" type="number">
-                    <v-icon color="error" slot="append" @click="showDialog = true">mdi-delete</v-icon>
+                    <v-icon color="error" slot="append" @click="deleteEntry">mdi-delete</v-icon>
                 </v-text-field>
             </v-col>
             <v-col cols="6">
@@ -12,41 +12,7 @@
             <v-col cols="12">
                 <v-textarea label="Description" v-model="description" auto-grow outlined rows="1"></v-textarea>
             </v-col>
-        </v-row>
-
-        <div class="text-center">
-            <v-dialog v-model="showDialog" width="500">
-                <v-card>
-                    <v-card-title class="text-h5 grey lighten-2">
-                        Delete XP Entry
-                    </v-card-title>
-
-                    <v-card-text>
-                        Are you sure you want to delete this entry?
-                        <v-row>
-                            <v-col cols="6">
-                                <v-text-field label="Amount" v-model="amount" type="number" disabled></v-text-field>
-                            </v-col>
-                            <v-col cols="6">
-                                <v-text-field label="Date" v-model="date" type="date" disabled></v-text-field>
-                            </v-col>
-                            <v-col cols="12">
-                                <v-textarea label="Description" v-model="description" auto-grow outlined rows="1" disabled></v-textarea>
-                            </v-col>
-                        </v-row>
-                    </v-card-text>
-
-                    <v-divider></v-divider>
-
-                    <v-card-actions class="justify-end">
-                        <v-btn color="error"
-                               @click="deleteEntry()">Delete</v-btn>
-                        <v-btn class="secondary"
-                               @click="showDialog = false">Close</v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-dialog>
-        </div>
+        </v-row>        
     </div>
 </template>
 
@@ -60,15 +26,12 @@
             return {
                 amount: this.entry.amount,
                 description: this.entry.description,
-                date: this.entry.date,
-                id: this.entry.id,
-                showDialog: false
+                date: this.entry.date
             }
         },
         methods: {
             deleteEntry() {
-                this.showDialog = false
-                this.$emit('deleteEntryEmit', { arrayName: 'xpEntries', object: this.entry })
+                this.$emit('deleteEntryEmit', this.entry)
             },
             updateEntry() {
                 var object = {
@@ -77,7 +40,7 @@
                     date: this.date,
                     id: this.entry.id
                 }
-                this.$emit('updateEntryEmit', { arrayName: 'xpEntries', object: object })
+                this.$emit('updateEntryEmit', object)
             }
         },
         watch: {
--- src/components/XPSection.vue
+++ src/components/XPSection.vue
@@ -4,7 +4,7 @@
             <h3 class="text-center">
                 XP Entries
                 <v-btn icon color="primary"
-                       @click="showDialog = true">
+                       @click="addDialog">
                     <v-icon>
                         mdi-plus
                     </v-icon>
@@ -12,41 +12,45 @@
             </h3>
             <XPEntryListItem v-for="entry in xpEntries" :key="entry.id"
                              :entry="entry"
-                             @deleteEntryEmit="deleteEntry($event)"
-                             @updateEntryEmit="updateEntry($event)">
-            </XPEntryListItem>
+                             @deleteEntryEmit="deleteDialog($event)"
+                             @updateEntryEmit="updateEntry($event)"></XPEntryListItem>
         </div>
         <div class="text-center">
-            <v-dialog v-model="showDialog" width="500">
+            <v-dialog v-model="dialog.show" width="500">
                 <v-card>
                     <v-card-title class="text-h5 grey lighten-2">
-                        Add XP Entry
+                        {{dialog.type}} XP Entry
                     </v-card-title>
 
                     <v-card-text>
                         <v-container>
-                            <v-row>
-                                <v-col cols="6">
-                                    <v-text-field label="Amount" type="number" v-model="amount"></v-text-field>
-                                </v-col>
-                                <v-col cols="6">
-                                    <v-text-field label="Date" type="date" v-model="date"></v-text-field>
-                                </v-col>
-                                <v-col cols="12">
-                                    <v-text-field label="Description" type="text" v-model="description"></v-text-field>
-                                </v-col>
-                            </v-row>
+                            <v-form ref="form"
+                                    v-model="valid"
+                                    :disabled="dialog.type == 'Delete'">
+                                <v-text-field label="Amount"
+                                              type="number"
+                                              v-model="amount"
+                                              ref="amount"
+                                              :rules="numberRules"
+                                              required></v-text-field>
+                                <v-text-field label="Date"
+                                              type="date"
+                                              v-model="date"></v-text-field>
+                                <v-text-field label="Description"
+                                              v-model="description"></v-text-field>
+                            </v-form>
                         </v-container>
                     </v-card-text>
-
 
                     <v-divider></v-divider>
 
                     <v-card-actions class="justify-end">
-                        <v-btn color="primary"
-                               @click="addEntry()">Add</v-btn>
+                        <v-btn color="primary" v-if="dialog.type == 'Add'" :disabled="!valid"
+                               @click="addEntry">Add</v-btn>
+                        <v-btn color="error" v-if="dialog.type == 'Delete'"
+                               @click="deleteEntry">Delete</v-btn>
                         <v-btn color="secondary"
-                               @click="showDialog = false">Close</v-btn>
+                               @click="dialog.show = false">Close</v-btn>
                     </v-card-actions>
                 </v-card>
             </v-dialog>
@@ -70,32 +74,85 @@
         },
         data() {
             return {
-                amount: 0,
+                dialog: {
+                    show: false,
+                    type: ''
+                },
                 availableXP: this.xp,
+                // Input Fields Start
+                amount: 1,
                 date: null,
                 description: '',
-                showDialog: false
+                entry: {
+                    amount: 1,
+                    date: null,
+                    description: '',
+                    id: ''
+                },
+                // Input Fields End
+                // Validation Start
+                numberRules: [
+                    v => !isNaN(parseInt(v)) && v >= 1 || 'Field may not be empty and value must be 1 or higher'
+                ],
+                valid: false
+                // Validation End
             }
         },
         methods: {
+            // CRUD Functions Start
             addEntry() {
-                var object = {
+                if (this.validate()) {
+                this.dialog.show = false
+                this.entry = {
                     amount: this.amount,
                     date: this.date,
                     description: this.description,
                     id: null
                 }
-                this.amount = 0
-                this.date = null
-                this.description = ''
-                this.showDialog = false
-                this.$emit('addEntryEmit', { arrayName: 'xpEntries', object: object })
+                this.$emit('addEntryEmit', { arrayName: 'xpEntries', object: this.entry })
+                }
             },
-            deleteEntry(entry) {
-                this.$emit('deleteEntryEmit', entry)
+            deleteEntry() {
+                this.dialog.show = false
+                this.$emit('deleteEntryEmit', { arrayName: 'xpEntries', object: this.entry })
             },
             updateEntry(entry) {
-                this.$emit('updateEntryEmit', entry)
+                this.$emit('updateEntryEmit', { arrayName: 'xpEntries', object: entry })
+            },
+            // CRUD Functions End
+            // Open Dialog Functions
+            addDialog() {
+                this.setDialog('Add')
+                this.entry = {
+                    amount: 1,
+                    date: null,
+                    description: '',
+                    id: ''
+                }
+                this.setInputs(this.entry)
+                setTimeout(() => {
+                    this.$refs.amount.focus()
+                }, 200)
+            },
+            deleteDialog(entry) {
+                this.entry = this.xpEntries.find(x => { return x.id == entry.id })
+                this.setInputs(this.entry)
+                this.setDialog('Delete')
+            },
+            setDialog(type) {
+                this.dialog = {
+                    show: true,
+                    type: type
+                }
+            },
+            setInputs(entry) {
+                this.amount = entry.amount
+                this.date = entry.date
+                this.description = entry.description
+            },
+            // Open Dialog Functions End
+            validate() {
+                return this.$refs.form.validate()
             }
         }
     }
