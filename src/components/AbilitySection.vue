46534c083abe6ea7e64cc942a682c7f5317f0ae6
Bryan Arriaga <turro92@hotmail.com>
2022-06-30 23:02:09 -0400

    Added AbilitySection.vue Imported AbilitySection to CharacterSheet Added additional properties to ability objects

---
+++ src/components/AbilitySection.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <v-row>
+            <v-col cols="4" v-for="a in abilities" :key="a.id">
+                {{a}}
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AbilitySection',
+        props: {
+            abilities: Array
+        }
+    }
+</script>
