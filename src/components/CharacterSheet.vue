0a547961553ea3c21a6fbc921e39aaba976fe37a
Bryan Arriaga <turro92@hotmail.com>
2022-11-02 23:02:25 -0400

    Added sienna to characters.json and added button to load her data

--- src/components/CharacterSheet.vue
+++ src/components/CharacterSheet.vue
@@ -635,7 +635,7 @@
             <v-col>
                 <v-btn color="primary" @click="setCharacterAs('belif')">Set as Belif</v-btn>
                 <v-btn color="primary" @click="setCharacterAs('cam')">Cam</v-btn>
-                <v-btn color="primary" @click="setCharacterAs('wilson')">Sienna</v-btn>
+                <v-btn color="primary" @click="setCharacterAs('sienna')">Sienna</v-btn>
             </v-col>
             <v-col>
                 <div>
